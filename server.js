fa7dcbc9397dd54eb348f4d9c0bc227c744ea746
paperpatch <patchen21@gmail.com>
2021-12-01 12:39:30 -0500

    realized that apiRoutes aren't needed. deleted related files

--- server.js
+++ server.js
@@ -22,9 +22,6 @@ app.use((req, res) => {
 db.connect(err => {
   if (err) throw err;
   console.log('Database connected.');
-  app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-  });
   promptUser();
 });
 
