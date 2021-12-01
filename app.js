5eec1f379966973bed37ef8791f808c784d6453a
paperpatch <patchen21@gmail.com>
2021-12-01 13:17:34 -0500

    deleted unused dependencies

--- app.js
+++ app.js
@@ -1,22 +1,5 @@
 const inquirer = require('inquirer');
-const express = require('express');
 const db = require('./db/connection');
-const apiRoutes = require('./routes/apiRoutes/');
-
-const PORT = process.env.PORT || 3001;
-const app = express();
-
-// Express middleware
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
-
-// Use apiRoutes
-app.use('/api', apiRoutes);
-
-// Default response for any other request (Not Found)
-app.use((req, res) => {
-  res.status(404).end();
-});
 
 // Start server after DB connection
 db.connect(err => {
